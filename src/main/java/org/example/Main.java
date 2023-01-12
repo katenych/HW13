45fdf6f3403c786a71e26b11d8be9c5a799bfa0b
katenych <kat.shcherbak@gmail.com>
2023-01-12 00:37:30 -0800

    Added new branch hw13

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,3 @@ package org.example;
 //all fields
 //default constructor
 //Створити 5 різних обєктів
-
-public class Main {
-    public static void main(String[] args) {
-        Person[] Person = {new Person("Britney", "Spears", 41),
-        new Person("John", "Daly", 36),
-        new Person("Anna", "female"),
-        new Person("Henry", "Wong",35, "male","1201 Sunset str."),
-        new Person("Tom", "male")};
-
-    }
-}
